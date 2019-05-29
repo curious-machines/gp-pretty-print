462d875ce574626a30ac222478cc857e7a437c5e
Kevin Lindsey <kevin@kevlindev.com>
2019-05-29 14:41:02 -0700

    Create a simple test
    This exercises each node type: source, line, indent, and text

---
+++ scratchpad/test.js
@@ -0,0 +1,71 @@
+import Printer from "../lib/Printer.js";
+
+const printer = new Printer();
+
+const tree = {
+    type: "source",
+    children: [
+        { type: "line", children: [
+            { type: "text", value: "while" },
+            { type: "text", value: " " },
+            { type: "text", value: "(" },
+            { type: "text", value: "true" },
+            { type: "text", value: ")" },
+            { type: "text", value: " " },
+            { type: "text", value: "{" }
+        ]},
+        { type: "indent", children: [
+            {
+                type: "line",
+                children: [
+                    {type: "text", value: "5"},
+                    {type: "text", value: " "},
+                    {type: "text", value: "+"},
+                    {type: "text", value: " "},
+                    {type: "text", value: "3"},
+                    {type: "text", value: ";"}
+                ]
+            },
+            { type: "indent", children: [
+                {
+                    type: "line",
+                    children: [
+                        {type: "text", value: "5"},
+                        {type: "text", value: " "},
+                        {type: "text", value: "+"},
+                        {type: "text", value: " "},
+                        {type: "text", value: "3"},
+                        {type: "text", value: ";"}
+                    ]
+                },
+                {
+                    type: "line",
+                    children: [
+                        {type: "text", value: "5"},
+                        {type: "text", value: " "},
+                        {type: "text", value: "+"},
+                        {type: "text", value: " "},
+                        {type: "text", value: "3"},
+                        {type: "text", value: ";"}
+                    ]
+                }
+            ]},
+            {
+                type: "line",
+                children: [
+                    {type: "text", value: "5"},
+                    {type: "text", value: " "},
+                    {type: "text", value: "+"},
+                    {type: "text", value: " "},
+                    {type: "text", value: "3"},
+                    {type: "text", value: ";"}
+                ]
+            }
+        ]},
+        { type: "line", children: [
+            { type: "text", value: "}" }
+        ]}
+    ]
+}
+
+console.log(printer.print(tree));
