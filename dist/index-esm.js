5a11247f2fa3aa439ec2aa669db992125f1c50a0
Kevin Lindsey <kevin@kevlindev.com>
2019-10-31 12:41:39 -0700

    Update and remove security vulnerabilities
    These were present only in development mode

--- dist/index-esm.js
+++ dist/index-esm.js
@@ -25,20 +25,55 @@ var Printer =
 function () {
   function Printer() {
     _classCallCheck(this, Printer);
+
+    this.indent = "";
   }
 
   _createClass(Printer, [{
     key: "print",
-    value: function print() {
-      return "";
+    value: function print(node) {
+      var _this = this;
+
+      var result = [];
+
+      switch (node.type) {
+        case "source":
+          node.children.forEach(function (child) {
+            result.push(_this.print(child));
+          });
+          break;
+
+        case "line":
+          node.children.forEach(function (child) {
+            result.push(_this.print(child));
+          });
+          result.push("\n");
+          break;
+
+        case "indent":
+          this.indent += "    ";
+          node.children.forEach(function (child) {
+            if (child.type !== "indent") {
+              result.push(_this.indent);
+            }
+
+            result.push(_this.print(child));
+          });
+          this.indent = this.indent.substring(0, this.indent.length - 4);
+          break;
+
+        case "text":
+          return node.value;
+
+        default:
+          throw new TypeError("unrecognized node type: ".concat(node.type));
+      }
+
+      return result.join("");
     }
   }]);
 
   return Printer;
 }();
-
-/**
- * @module gp-pretty-print
- */
 
 export { Printer };
--- dist/index-umd.js
+++ dist/index-umd.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (global = global || self, factory(global.GpPrettyPrint = {}));
-}(this, function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -31,24 +31,59 @@
   function () {
     function Printer() {
       _classCallCheck(this, Printer);
+
+      this.indent = "";
     }
 
     _createClass(Printer, [{
       key: "print",
-      value: function print() {
-        return "";
+      value: function print(node) {
+        var _this = this;
+
+        var result = [];
+
+        switch (node.type) {
+          case "source":
+            node.children.forEach(function (child) {
+              result.push(_this.print(child));
+            });
+            break;
+
+          case "line":
+            node.children.forEach(function (child) {
+              result.push(_this.print(child));
+            });
+            result.push("\n");
+            break;
+
+          case "indent":
+            this.indent += "    ";
+            node.children.forEach(function (child) {
+              if (child.type !== "indent") {
+                result.push(_this.indent);
+              }
+
+              result.push(_this.print(child));
+            });
+            this.indent = this.indent.substring(0, this.indent.length - 4);
+            break;
+
+          case "text":
+            return node.value;
+
+          default:
+            throw new TypeError("unrecognized node type: ".concat(node.type));
+        }
+
+        return result.join("");
       }
     }]);
 
     return Printer;
   }();
 
-  /**
-   * @module gp-pretty-print
-   */
-
   exports.Printer = Printer;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-}));
+})));
